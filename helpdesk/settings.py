7cf9f82f44db97eb65434c56c9386a971f7781f1
Eduard Alekseiev <edikaleks43+github@gmail.com>
2024-02-19 12:33:45 +0200

    Add pagination

--- helpdesk/settings.py
+++ helpdesk/settings.py
@@ -161,5 +161,7 @@ LOGGING = {
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    )
+    ),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 100,
 }
